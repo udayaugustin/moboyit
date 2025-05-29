d51d76abe3a4b87012403610e3993ec6b1f3c2c5
Uday Augustin <udayaugustin@gmail.com>
2025-05-29 17:47:07 +0530

    chore: move vite and react-swc from devDependencies to dependencies and simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  server: {
-    port: 8080,
-    host: true
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false
-  }
 });
